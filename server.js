caaf1a7187d0867c1665b6ce7b7dea1f4cb28937
BakerSalem <103444608+BakerSalem@users.noreply.github.com>
2025-12-25 15:37:24 +0200

    V1

--- server.js
+++ server.js
@@ -47,5 +47,4 @@ new FairWarManager(io);
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
 });
