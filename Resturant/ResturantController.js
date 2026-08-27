aef01bcd581d2377c8a4e41bb24c336eaab2ee74
Mohammed ALGorrah <87028441+Mohammed-ALGorrah@users.noreply.github.com>
2026-08-27 14:40:27 +0300

    edit resturant Server

--- Resturant/ResturantController.js
+++ Resturant/ResturantController.js
@@ -1,4 +1,3 @@
-
 const MainController = require('../MainConrtollers/mainController');
 
 class ResturantController extends MainController {
@@ -15,19 +14,31 @@ class ResturantController extends MainController {
      */
     bind(socket) {
 
-            socket.on('New Customer', (data) => {
-                this.SendSocketALL(socket, 'New Customer', data, 'New Customer sent', 'New Customer failed');
-                console.log(data)
-            });
+        socket.on('New Customer', (data) => {
+            this.SendSocketALL(socket, 'New Customer', data, 'New Customer sent', 'New Customer failed');
+            console.log(data)
+        });
 
-            socket.on('NextWord', (data) => {
-                this.SendSocketALL(socket, 'NextWord', data, 'NextWord sent', 'NextWord failed');
-                console.log(data)
-            });
+        socket.on('next sentence', (data) => {
+            this.SendSocketALL(socket, 'next sentence', data, 'next sentence sent', 'next sentence failed');
+            console.log(data)
+        });
+
+        socket.on('repeat sentence', (data) => {
+            this.SendSocketALL(socket, 'repeat sentence', data, 'repeat sentence sent', 'repeat sentence failed');
+            console.log(data)
+        });
+
+        socket.on('scenario', (data) => {
+            this.SendSocketBroadcast(socket, 'scenario', data, 'scenario sent', 'scenario failed');
+        });
+
+        socket.on('reset', (data) => {
+            this.SendSocketALL(socket, 'reset', data, 'reset sent', 'reset failed');
+            console.log(data)
+        });
+
     }
-
-
-
 }
 
 
