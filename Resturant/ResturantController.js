614d7e8e0e03b321ab718b09de6e17ae296323a7
Mohammed ALGorrah <87028441+Mohammed-ALGorrah@users.noreply.github.com>
2026-08-29 14:10:11 +0300

    Update ResturantController.js

--- Resturant/ResturantController.js
+++ Resturant/ResturantController.js
@@ -16,17 +16,14 @@ class ResturantController extends MainController {
 
         socket.on('New Customer', (data) => {
             this.SendSocketALL(socket, 'New Customer', data, 'New Customer sent', 'New Customer failed');
-            console.log(data)
         });
 
         socket.on('next sentence', (data) => {
             this.SendSocketALL(socket, 'next sentence', data, 'next sentence sent', 'next sentence failed');
-            console.log(data)
         });
 
         socket.on('repeat sentence', (data) => {
             this.SendSocketALL(socket, 'repeat sentence', data, 'repeat sentence sent', 'repeat sentence failed');
-            console.log(data)
         });
 
         socket.on('scenario', (data) => {
@@ -35,7 +32,6 @@ class ResturantController extends MainController {
 
         socket.on('reset', (data) => {
             this.SendSocketALL(socket, 'reset', data, 'reset sent', 'reset failed');
-            console.log(data)
         });
 
     }
